10911668c208e6dc41bfd5e252d992e1cab0b517
KarinKwint <83405911+KarinKwint@users.noreply.github.com>
2021-06-15 12:47:43 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+//First part
+const paint = function (action) {
+    console.log('The wall has been painted ' + action);
+}
+
+const mainColor = 'red';
+const secondColor = 'green';
+
+paint(secondColor);
+
+//Second part
+const wall = function (item) {
+    console.log("item + has been painted green");
+}
+
+const leftWall = "The southeast";
